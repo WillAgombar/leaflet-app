f3af57c45e9ebecc31aba3ba4a973fdd6e53bb05
WillAgombar <willagombar@gmail.com>
2026-03-25 10:40:18 +0000

    Redirect home route to campaigns page

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\MapRouteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/campaigns');
 
 Route::get('/map', [MapRouteController::class, 'show'])->name('map-routes.show');
 Route::post('/map-routes', [MapRouteController::class, 'store'])->name('map-routes.store');
