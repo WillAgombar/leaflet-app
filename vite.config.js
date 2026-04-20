a1762c88ed1c673aee9efa7eb120fee8ce1f83a4
WillAgombar <willagombar@gmail.com>
2026-04-20 21:17:49 +0100

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/pages/leaflet-tracker.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/pages/leaflet-tracker.js', 'resources/js/pages/assignment-show.js'],
             refresh: true,
         }),
         tailwindcss(),
